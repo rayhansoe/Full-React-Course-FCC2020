fe24d179f3b8046c339b07ad5bc8b243d3c5dac7
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-08-27 12:36:30 +0700

    UseStateArray.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { UseStateArray } from "./tutorial/1-useState/setup/1-useState"
+
 const App = () => {
 	return (
 		<div className='container'>
-			<h2>Basic useState</h2>
+			{/* <h2>Basic useState</h2> */}
+			<UseStateArray />
 		</div>
 	)
 }
--- src/tutorial/1-useState/setup/1-useState.jsx
+++ src/tutorial/1-useState/setup/1-useState.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { data } from "../../../data"
 
 export const UseStateBasics = () => {
 	const state = useState(1)
@@ -8,6 +9,39 @@ export const UseStateBasics = () => {
 			{console.log(state[0])}
 			{console.log(state[1])}
 			{console.log(state[1](1))}
+		</>
+	)
+}
+
+export const UseStateArray = () => {
+	const [people, setPeople] = useState(() => data)
+
+	const removeItems = () => setPeople(() => [])
+
+	const removeItem = id => {
+		const newArray = people.filter(person => person.id !== id)
+
+		setPeople(() => newArray)
+	}
+
+	const peopleList = people.map(person => {
+		const { id, name } = person
+
+		return (
+			<div className='item' key={id}>
+				<h2>{name}</h2>
+				<button onClick={() => removeItem(id)}>remove</button>
+			</div>
+		)
+	})
+
+	return (
+		<>
+			{/* <h1>UseState Array Example</h1> */}
+			{peopleList}
+			<button className='btn' onClick={removeItems}>
+				Clear Items
+			</button>
 		</>
 	)
 }
