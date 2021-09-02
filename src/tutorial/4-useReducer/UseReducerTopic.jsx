045eeddca9aadf030a7205c74b064d7a22b9af13
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-09-03 00:29:10 +0700

    add inputRef, set default value for isFetchLoading to be false, import MyInput, import useRef, add handleSubmit function, use dynamic url, change the conditional return for page still loading, change conditional return for default return, and fix what needs to fixed.

--- src/tutorial/4-useReducer/UseReducerTopic.jsx
+++ src/tutorial/4-useReducer/UseReducerTopic.jsx
@@ -1,15 +1,14 @@
-import React, { useEffect, useReducer } from "react"
-
-import { checkStatus } from "../../tools"
-import MyItem from "./component/MyItem"
+import React, { useEffect, useReducer, useRef } from "react"
 
 import { reducer } from "./reducer"
+import { checkStatus } from "../../tools"
 
-const url = "https://api.github.com/users"
+import MyItem from "./component/MyItem"
+import MyInput from "./component/MyInput"
 
 // default state
 const defaultState = {
-	isFetchLoading: true,
+	isFetchLoading: false,
 	isError: false,
 	users: [],
 	url: "",
@@ -19,6 +18,7 @@ const defaultState = {
 // main component
 const UseReducerTopic = () => {
 	const [state, dispatch] = useReducer(reducer, defaultState)
+	const inputRef = useRef(null)
 
 	// user list
 	const getUsersList = () =>
@@ -26,10 +26,26 @@ const UseReducerTopic = () => {
 			return <MyItem key={user.id} user={user} />
 		})
 
+	const handleSubmit = e => {
+		e.preventDefault()
+		dispatch({
+			type: "ADD_URL",
+			payload: {
+				url: inputRef.current.value,
+			},
+		})
+		dispatch({
+			type: "FETCH_STATUS",
+			payload: {
+				isFetchLoading: true,
+			},
+		})
+	}
+
 	// the side effect for data fetching
 	useEffect(() => {
 		const check = async () => {
-			const result = await checkStatus(url).catch(err => console.log(err))
+			const result = await checkStatus(state.url).catch(err => console.log(err))
 			result &&
 				dispatch({
 					type: "FIRST_RENDER",
@@ -46,18 +62,24 @@ const UseReducerTopic = () => {
 
 			setTimeout(() => {
 				dispatch({
-					type: "LOADING_FINISHED",
+					type: "FETCH_STATUS",
 					payload: {
 						isFetchLoading: false,
 					},
 				})
 			}, 500)
 		}
-		!state.users.length && check()
-	}, [state.users.length])
 
-	// when the page is loading
-	if (state.isFetchLoading) {
+		if (!state.users.length && state.url) {
+			check()
+		}
+	}, [state.isPageLoading, state.url, state.users.length])
+
+	// when the page still loading
+	if (state.isPageLoading) {
+		setTimeout(() => {
+			dispatch({ type: "LOADING_FINISHED", payload: { isPageLoading: false } })
+		}, 500)
 		return (
 			<>
 				<h1>Loading....</h1>
@@ -78,8 +100,33 @@ const UseReducerTopic = () => {
 	// when the loading is finished
 	return (
 		<>
-			<h3>GitHub Users</h3>
-			<ul className='users'>{getUsersList()}</ul>
+			<h3 style={{ marginBottom: "40px" }}>UseEffect | Fetch | UseReducer</h3>
+			<section>
+				<form className='form' onSubmit={handleSubmit}>
+					<MyInput
+						nameForId='url'
+						status={state.url ? true : false}
+						label='Url : '
+						ref={inputRef}
+						type='url'
+					/>
+					<button type='submit' onClick={handleSubmit}>
+						add url
+					</button>
+				</form>
+			</section>
+			<section>
+				{state.isFetchLoading ? (
+					<h3>Loading...</h3>
+				) : (
+					state.url && (
+						<>
+							<h3>GitHub Users</h3>
+							<ul className='users'>{getUsersList()}</ul>
+						</>
+					)
+				)}
+			</section>
 		</>
 	)
 }
