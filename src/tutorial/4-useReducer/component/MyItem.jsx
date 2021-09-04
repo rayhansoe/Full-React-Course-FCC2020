48f7077903eecfb06190a2681a86798ff7a3cb57
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-09-04 10:52:49 +0700

    fix what needs to fixed.

--- src/tutorial/4-useReducer/component/MyItem.jsx
+++ src/tutorial/4-useReducer/component/MyItem.jsx
@@ -1,15 +1,11 @@
 const MyItem = ({ user, handleClick }) => {
 	const { id, avatar_url, html_url, login } = user
 	return (
-		<li key={id}>
+		<li key={id} className='item'>
+			<img src={avatar_url} alt={login} />
 			<a href={html_url} target='_blank' rel='noreferrer'>
-				<img src={avatar_url} alt={login} />
+				{login}
 			</a>
-			<div>
-				<a href={html_url} target='_blank' rel='noreferrer'>
-					{login}
-				</a>
-			</div>
 			<button onClick={() => handleClick(id)}>remove</button>
 		</li>
 	)
