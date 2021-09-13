5f802c716e3bc803b087abd9f71d8a105559ee14
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-09-13 22:03:00 +0700

    add layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React, { lazy, Suspense } from "react"
+import { Layout } from 'antd'
+import React, { lazy, Suspense } from 'react'
 
-const Index = lazy(() => import("./tutorial/5-React-Router/index"))
+const Index = lazy(() => import('./tutorial/5-React-Router/index'))
 
 const App = () => {
 	return (
-		<div className='container'>
-			<Suspense fallback={<h1>Loading...</h1>}>
-				<Index />
-			</Suspense>
-		</div>
+		<Layout>
+			<div className='container'>
+				<Suspense fallback={<h1>Loading...</h1>}>
+					<Index />
+				</Suspense>
+			</div>
+		</Layout>
 	)
 }
 
