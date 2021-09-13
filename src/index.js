24d1a9050245f58386133fa4ae293f2719d3939d
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-09-13 22:17:15 +0700

    ReactDOM.hydrate

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'antd/dist/antd.css'
 import './index.css'
 const App = lazy(() => import('./App'))
 
-ReactDOM.render(
+ReactDOM.hydrate(
 	<React.StrictMode>
 		<Suspense fallback={<h1>Loading...</h1>}>
 			<App />
