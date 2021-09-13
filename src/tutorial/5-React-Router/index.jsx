80991cf10dec496473d2cda210f2cf860d15f778
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-09-13 22:03:29 +0700

    make more stylish.

--- src/tutorial/5-React-Router/index.jsx
+++ src/tutorial/5-React-Router/index.jsx
@@ -1,12 +1,15 @@
-import React, { lazy, Suspense } from "react"
-import { HashRouter as Router, Route, Switch, Link } from "react-router-dom"
+import { Col, Row, Typography, Card } from 'antd'
+import React, { lazy, Suspense } from 'react'
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
-const BookList = lazy(() => import("../0-Basics/BookList"))
-const NotFound = lazy(() => import("../NotFound"))
-const UseStateArray = lazy(() => import("../1-useState/useStateTopics"))
-const Main = lazy(() => import("../3-conditionalRender/conditionalRender"))
-const UseReducerTopic = lazy(() => import("../4-useReducer/UseReducerTopic"))
-const UseEffectFecthData = lazy(() => import("../2-useEffect/useEffectTopics"))
+const BookList = lazy(() => import('../0-Basics/BookList'))
+const NotFound = lazy(() => import('../NotFound'))
+const UseStateArray = lazy(() => import('../1-useState/useStateTopics'))
+const Main = lazy(() => import('../3-conditionalRender/conditionalRender'))
+const UseReducerTopic = lazy(() => import('../4-useReducer/UseReducerTopic'))
+const UseEffectFecthData = lazy(() => import('../2-useEffect/useEffectTopics'))
+
+const { Title } = Typography
 
 const Index = () => {
 	return (
@@ -31,31 +34,64 @@ export default Index
 const Home = () => {
 	return (
 		<div>
-			<h2>Home</h2>
-			<nav>
-				<ul className='item'>
-					<li>
-						<Link to='/'>Home</Link>
-					</li>
-					<li>
+			<Title>Home</Title>
+			<Row gutter={[16, 16]}>
+				<Col span={12}>
+					<Card
+						title={
+							<Link to='/booklist'>
+								<Title level={3}>BookList</Title>
+							</Link>
+						}
+						bordered={false}>
 						<Link to='/booklist'>BookList</Link>
-					</li>
-					<li>
+					</Card>
+				</Col>
+				<Col span={12}>
+					<Card
+						title={
+							<Link to='/usestate'>
+								<Title level={3}>UseState Topic</Title>
+							</Link>
+						}
+						bordered={false}>
 						<Link to='/usestate'>UseState Topic</Link>
-					</li>
-				</ul>
-				<ul className='item'>
-					<li>
+					</Card>
+				</Col>
+				<Col span={12}>
+					<Card
+						title={
+							<Link to='/useeffect'>
+								<Title level={3}>UseEffect Topic</Title>
+							</Link>
+						}
+						bordered={false}>
 						<Link to='/useeffect'>UseEffect Topic</Link>
-					</li>
-					<li>
+					</Card>
+				</Col>
+				<Col span={12}>
+					<Card
+						title={
+							<Link to='/conditionalrender'>
+								<Title level={3}>Conditional Render</Title>
+							</Link>
+						}
+						bordered={false}>
 						<Link to='/conditionalrender'>Conditional Render</Link>
-					</li>
-					<li>
+					</Card>
+				</Col>
+				<Col span={24}>
+					<Card
+						title={
+							<Link to='/usereducer'>
+								<Title level={3}>UseReducer Topic</Title>
+							</Link>
+						}
+						bordered={false}>
 						<Link to='/usereducer'>UseReducer Topic</Link>
-					</li>
-				</ul>
-			</nav>
+					</Card>
+				</Col>
+			</Row>
 		</div>
 	)
 }
